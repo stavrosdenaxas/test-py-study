11f0b60f77bbffe0ce09b3c136a01221fa2f2ee1
stavrosdenaxas <pox@google.com>
2024-05-03 08:26:20 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='test',
+    version='0.1.0',
+    packages=[''],
+    url='ultrasandpit.com',
+    license='',
+    author='stavrosdenaxas',
+    author_email='pox@google.com',
+    description='test'
+)
